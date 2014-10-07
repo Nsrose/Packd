dd9bf88483dbe5951fabcf7be1878d12868f6cf2
nsrose <nsrose@berkeley.edu>
2014-10-07 11:56:10 -0700

    set up web app

--- packdWebApp/static/js/script.js
+++ packdWebApp/static/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-    // Get data from Firebase
-    var fireRef = new Firebase("https://radiant-torch-8194.firebaseio.com/");
+    // Here's our firebase url:
+    var fireRef = new Firebase("https://packd.firebaseio.com/");
+    
+    // This function will be called when the data is changed in the server
     fireRef.on('value', function (snapshot) {
-        var dataText = snapshot.child("user_data_text").val();
+        var dataText = snapshot.child("test_text").val();
         $("#test").text(dataText);
     }, function (errorObject) {
         var dataText = "Sorry, we couldn't get any data right now.";
