f3c6658c9297a88cdf5394c6e0a19c74bc03f8f1
nsrose <nsrose@berkeley.edu>
2015-01-23 12:12:27 -0800

    more options

--- packdWebApp/main.py
+++ packdWebApp/main.py
@@ -28,6 +28,13 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template("index.html")
         self.response.out.write(template.render(template_values))
 
+class InfoHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = jinja_environment.get_template("info.html")
+        self.response.out.write(template.render(template_values))
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/info', InfoHandler),
 ], debug=True)
