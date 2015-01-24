b33dde7e6997927be5f71cd2db9c3313dec9e059
nsrose <nsrose@berkeley.edu>
2015-01-23 18:00:34 -0800

    updating feedback

--- packdWebApp/static/js/script.js
+++ packdWebApp/static/js/script.js
@@ -8,6 +8,20 @@ var days = {
     "6":"Saturday",
 }
 
+var ints_to_strings = {
+    "1":"Not Crowded",
+    "2":"Mildly Crowded",
+    "3":"Very Crowded",
+    "4":"Extreme",
+}
+
+var strings_to_ints = {
+    "Not Crowded":1,
+    "Mildly Crowded":2,
+    "Very Crowded":3,
+    "Extreme":4,
+}
+
 // Firebase url
 var fireRef = new Firebase("https://packd.firebaseio.com/");
 
@@ -48,14 +62,35 @@ function distance(lon1, lat1, lon2, lat2) {
 function checkLoadFactor(snapshot) {
     var size = snapshot.child("Size").val();
     if (size > LOAD_FACTOR) {
-        var feedback = snapshot.child("feedback").val();
-        refactor();
+        refactor(snapshot);
     }
 }
 
 // Refactors the tables for data to restructure json.
-function refactor() {
+function refactor(snapshot) {
+    var feedback = snapshot.child("feedback").val();
+    for (var day in feedback) {
+        for (var hour in feedback[day]) {
+            var dataPoints = feedback[day][hour];
+            console.log(dataPoints);
+            var denom = 0;
+            var num = 0;
+            if (dataPoints != "none") {
+                for (var dataPoint in dataPoints) {
+                    denom += 1;
+                    num += weight(dataPoints[dataPoint]["measure"]);
+                }
+                var average = num / denom;
+                console.log(average);
+            }
+        }
+    }
+}
 
+// Given a heuristic string, returns an integer value representing
+// that string.
+function weight(string) {
+    return strings_to_ints[string];
 }
 
 $(document).ready(function(){
---
+++ weights.py
@@ -0,0 +1,21 @@
+import json
+from pprint import pprint
+
+
+## Will overwrite Feedback.json: CAUTION##
+def weights():
+    json_data = open("feedback.json")
+    data = json.load(json_data)
+    i = 0
+    for key in data:
+        i += 1
+        for value in data[key]:
+            i += 1
+            measure = data[key][value]
+            new = {
+                "measure":measure,
+                "weight":100,
+            }
+            data[key][value] = {}
+            data[key][value]["current_average"] = new
+    json.dump(data, open("feedback.json", 'wb'))
