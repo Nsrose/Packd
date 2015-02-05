7b0b15b1d89369b66283babd92aaa294abc903da
Ollie O'Donnell <oliverodaa@gmail.com>
2015-02-05 09:12:38 -0800

    chart added, functionality in!

--- packdWebApp/static/js/visualEffects.js
+++ packdWebApp/static/js/visualEffects.js
@@ -1,16 +1,25 @@
+generateGraph();
+
 $(document).ready(function(){
 
 	$("#time").text(new Date());
 	$("#disagree").hide();
 	$("#disagree_button").click(function() {
+		$("#data_container").show();
+		$("#future_graph").hide();
 		$("#disagree").show();
 		$("#data").hide();
         $("#percentage").hide();
 	});
 	$("#current_button").click(function() {
+		$("#data_container").show();
+		$("#future_graph").hide();
 		$("#disagree").hide();
 		$("#data").show();
         $("#percentage").show();
 	});
-	// $("#graph_button").click(generateGraph());
+	$("#predict_button").click(function() {
+		$("#future_graph").show();		
+		$("#data_container").hide();
+	});
 })
