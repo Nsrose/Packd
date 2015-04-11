97a7d514446d96133db07e55ad8dabace333c08a
nsrose <nsrose@berkeley.edu>
2015-04-11 16:41:11 -0700

    prepare for staging

--- packdStaging/static/js/graph.js
+++ packdStaging/static/js/graph.js
@@ -1,4 +1,5 @@
-var fireRef = new Firebase("https://packd.firebaseio.com/");
+
+var fireRef = new Firebase(fireString);
 
 var days = {
     "0":"Sunday",
@@ -24,13 +25,18 @@ function generateGraph() {
         var y_axis_values = [];
         var x_axis_values = [];
         var cur_int_val = 0;
-        var prev_int_val;
+        var prev_int_val = 0;
+        prev_prev_int_val = 0;
         for (var key in dayData) {
             cur_int_val = strings_to_ints[dayData[key]["current_average"]["measure"]];
             if (key >= hour) {
                 x_axis_values.push(key);
-                y_axis_values.push(cur_int_val);
+                y_axis_values.push(
+                    ((prev_prev_int_val/4 + prev_int_val/2 + cur_int_val)/6).toFixed(2));
+                // 1/4 of people stay for >2 hours, 1/2 for >1 hour
             }
+            prev_prev_int_val = prev_int_val;
+            prev_int_val = cur_int_val;
         }
         var myChartData = {
         labels: x_axis_values,
@@ -41,8 +47,11 @@ function generateGraph() {
                 data: y_axis_values,
             },
         ]};
+        var options = {
+            pointDot:false,
+        }
         var ctx = $("#myChart").get(0).getContext("2d");
-        var myNewChart = new Chart(ctx).Line(myChartData);
+        var myNewChart = new Chart(ctx).Line(myChartData, options);
         $("#future_graph").hide();
 	}, function (errorObject) {
         console.log("error in graph generation");
